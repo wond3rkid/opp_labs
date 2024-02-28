ac372e8abb5ec41abb85b4b0317ffed93e8f5ba3
savilovaae <a.savilova@g.nsu.ru>
2024-02-28 23:46:15 +0700

    add main, CMakeLists, process files

---
+++ second/process.c
@@ -0,0 +1,14 @@
+#include "process.h"
+#include <stdio.h>
+
+void fillMatrix(double **matrix_A, double *matrix_B, size_t N) {
+    for (size_t i = 0; i < N; i++) {
+        for (size_t j = 0; j < N; j++) {
+            matrix_A[i][j] = (i == j) ? 2 : 1;
+        } matrix_B[i] = N + 1;
+    }
+}
+
+void iterationMethod() {
+
+}
---
+++ second/process.h
@@ -0,0 +1,9 @@
+#ifndef SECOND_PROCESS_H
+#define SECOND_PROCESS_H
+#include <stdio.h>
+
+void fillMatrix(double **matrix_A, double *matrix_B, size_t N);
+
+void iterationMethod();
+
+#endif //SECOND_PROCESS_H
