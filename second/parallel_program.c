ec5c74f1c84777cf6f0e9a2ef2996d0ccea1d696
savilovaae <a.savilova@g.nsu.ru>
2024-03-06 23:50:54 +0700

    start parallel program step 1 try to win memory leak with mul matrix vector

--- second/parallel_program.c
+++ second/parallel_program.c
@@ -1,43 +1,47 @@
 #include "parallel_program.h"
 
-void parallel_fill_matrix(double *approximation, size_t N) {
+void p_fill_initial_approximation(double *approximation, size_t N) {
+    int i;
+#pragma omp parallel for private(i)
+    for (i = 0; i < N; i++) {
+        approximation[i] = 0;
+    }
+}
+
+void p_fill_matrix_vector(double **matrix, double *vector, size_t N) {
+    int i, j;
+#pragma omp parallel for shared(matrix, vector, N) private(i, j) default (none)
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < N; j++) {
+            matrix[i][j] = (i == j) ? 2 : 1;
+        }
+        vector[i] = (double) N + 1;
+    }
+}
+
+bool p_is_solved(const double **matrix, const double *vector, double *curr_approximation, size_t N) {
+    double numerator_sqrt = p_get_vector_sqrt(vector, N);
+    double *denominator = p_multiplication_matrix_vector(matrix, curr_approximation, N);
+    p_subtracting_vectors(denominator, vector, N);
+    double denominator_sqrt = p_get_vector_sqrt(denominator, N);
+    free(denominator);
+    return denominator_sqrt / numerator_sqrt < Epsilon;
+}
+
+double *p_multiplication_matrix_vector(const double **matrix, double *curr_approximation, size_t N) {
 
 }
 
-void parallel_fill_matrix_vector(double **matrix, double *vector, size_t N) {
+void p_subtracting_vectors(double *curr, const double *vector, size_t N);
 
-}
+double *p_multiplication_tau_vector(const double *vector, size_t N);
 
-bool is_solved_parallel(const double **matrix, const double *vector, double *curr_approximation, size_t N) {
+double p_get_vector_sqrt(const double *vector, size_t N);
 
-}
+double *p_get_next_x(const double **matrix, const double *vector, double *curr_approximation, size_t N);
 
-double *parallel_multiplication_matrix_vector(const double **matrix, double *curr_approximation, size_t N) {
+void p_preparation_perfomance_free(size_t N);
 
-}
+void p_solve_equations(const double **matrix, const double *vector, double *initial_approximation, size_t N);
 
-void parallel_subtracting_vectors(double *curr, const double *vector, size_t N) {
-
-}
-
-double *parallel_multiplication_tau_vector(const double *vector, size_t N) {
-
-}
-
-double get_vector_sqrt_parallel(const double *vector, size_t N);
-
-double *get_next_x_parallel(const double **matrix, const double *vector, double *curr_approximation, size_t N) {
-
-}
-
-void preparation_perfomance_free_parallel(size_t N) {
-
-}
-
-void parallel_solve_equations(const double **matrix, const double *vector, double *initial_approximation, size_t N) {
-
-}
-
-void print_result_for_parallel(double *res, size_t N) {
-
-}
+void p_print_result(double *res, size_t N);
--- second/parallel_program.h
+++ second/parallel_program.h
@@ -9,26 +9,26 @@
 #define Epsilon 0.00001
 #define Tau 0.00001
 
-void parallel_fill_initial_approximation(double *approximation, size_t N);
+void p_fill_initial_approximation(double *approximation, size_t N);
 
-void parallel_fill_matrix_vector(double **matrix, double *vector, size_t N);
+void p_fill_matrix_vector(double **matrix, double *vector, size_t N);
 
-bool is_solved_parallel(const double **matrix, const double *vector, double *curr_approximation, size_t N);
+bool p_is_solved(const double **matrix, const double *vector, double *curr_approximation, size_t N);
 
-double *parallel_multiplication_matrix_vector(const double **matrix, double *curr_approximation, size_t N);
+double *p_multiplication_matrix_vector(const double **matrix, double *curr_approximation, size_t N);
 
-void parallel_subtracting_vectors(double *curr, const double *vector, size_t N);
+void p_subtracting_vectors(double *curr, const double *vector, size_t N);
 
-double *parallel_multiplication_tau_vector(const double *vector, size_t N);
+double *p_multiplication_tau_vector(const double *vector, size_t N);
 
-double get_vector_sqrt_parallel(const double *vector, size_t N);
+double p_get_vector_sqrt(const double *vector, size_t N);
 
-double *get_next_x_parallel(const double **matrix, const double *vector, double *curr_approximation, size_t N);
+double *p_get_next_x(const double **matrix, const double *vector, double *curr_approximation, size_t N);
 
-void preparation_perfomance_free_parallel(size_t N);
+void p_preparation_perfomance_free(size_t N);
 
-void parallel_solve_equations(const double **matrix, const double *vector, double *initial_approximation, size_t N);
+void p_solve_equations(const double **matrix, const double *vector, double *initial_approximation, size_t N);
 
-void print_result_for_parallel(double *res, size_t N);
+void p_print_result(double *res, size_t N);
 
 #endif //SECOND_PARALLEL_PROGRAM_H
