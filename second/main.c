4db9f2f3b359b0e12f97788d7e6ce8136388675a
savilovaae <a.savilova@g.nsu.ru>
2024-03-07 11:05:21 +0700

    all work

--- second/main.c
+++ second/main.c
@@ -3,17 +3,16 @@
 #include <time.h>
 
 int main(int argc, char **argv) {
-    int N = 100;
+    int N = 10000;
 
     clock_t start = clock(), end;
     preparation_perfomance_free(N);
     end = clock();
     printf("Time taken for non-parallel: %f seconds\n", (double) (end - start) / CLOCKS_PER_SEC);
 
-    int start1 = clock();
+    double start1 = omp_get_wtime();
     p_preparation_perfomance_free(N);
-    int end1 = clock();
-    printf("Time taken for parallel: %f seconds\n", (double) (end1 - start1) / CLOCKS_PER_SEC);
-
+    double end1 = omp_get_wtime();
+    printf("Time taken for parallel: %f seconds\n", end1 - start1);
     return 0;
 }
