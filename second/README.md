5f923acf3670d50de298058c2fdf00193dd5e199
savilovaae <a.savilova@g.nsu.ru>
2024-03-27 20:55:59 +0700

    iam code cleaner

--- second/README.md
+++ second/README.md
@@ -2,26 +2,15 @@
 ### «Параллельная реализация решения системы линейных алгебраических уравнений с помощью OpenMP»
 
 #### Замеры времени работы программы при разном количество потоков
-_**N = 10 000**_
+_**N = 5 000**_
 
-1 Вариант:
 
-| threads | time taken, seconds |
-| ------- |---------------------|
-| 1       | 68.715656           |
-| 2       | 34.928088           |
-| 3       | 24.723057           |
-| 4       | 19.951360           |
-| 5       | 17.093051           |
-| 6       | 15.586623           |
-| 7       | 19.816114           |
-| 8       | 18.459846           |
-| 9       | 16.841057           |
-| 10      | 16.107252           |
-| 11      | 15.626192           |
-| 12      | 15.003557           |
-
-#### Команда для компиляции. CMakeLists.txt - не рабочий вариант
+##### Команда для компиляции. CMakeLists.txt - не рабочий вариант
 ```bash
- gcc -fopenmp main.c sequential_program.c parallel_for_program.c parallel_section_program.c -o main.out -lm
+gcc -fopenmp main.c sequential_program.c parallel_for_program.c parallel_section_program.c -o main.out -lm`
+```
+##### Команда для задания количества потоков при исполнении программы
+```bash
+export OMP_NUM_THREADS=12
+./main.out 10000
 ```
--- second/parallel_section_program.c
+++ second/parallel_section_program.c
@@ -18,25 +18,16 @@ void s_fill_matrix_vector(double **matrix, double *vector, size_t N) {
 bool s_is_solved(const double **matrix, const double *vector, double *curr_approximation, size_t N) {
     double numerator_sqrt = s_get_vector_sqrt(vector, N);
     double *denominator = malloc(sizeof(denominator) * N);
-    //fprintf(stderr, "HERE");
     s_multiplication_matrix_vector(matrix, curr_approximation, denominator, N);
     s_subtracting_vectors(denominator, vector, N);
     double denominator_sqrt = s_get_vector_sqrt(denominator, N);
     free(denominator);
-    // there is 1 thread
-    // error is here
-
-    //fprintf(stderr, "%f %f \n", denominator_sqrt, denominator_sqrt / numerator_sqrt );
     return denominator_sqrt / numerator_sqrt < Epsilon;
 }
 
 void s_multiplication_matrix_vector(const double **matrix, const double *vector, double *res, size_t N) {
-// there is 12 threads
 #pragma omp for
     for (int i = 0; i < N; i++) {
-/*        if (i < 15) {
-            fprintf(stderr, "s %d - %d thread\n", i, omp_get_thread_num());
-        }*/
         for (int j = 0; j < N; j++) {
             res[i] += matrix[i][j] * vector[j];
         }
@@ -44,7 +35,6 @@ void s_multiplication_matrix_vector(const double **matrix, const double *vector,
 }
 
 void s_subtracting_vectors(double *curr, const double *vector, size_t N) {
-    // there is 12 threads
 #pragma omp for
     for (int i = 0; i < N; i++) {
         curr[i] -= vector[i];
@@ -52,7 +42,6 @@ void s_subtracting_vectors(double *curr, const double *vector, size_t N) {
 }
 
 void s_multiplication_tau_vector(const double *vector, double *result, size_t N) {
-    // there is 12 threads
 #pragma omp for
     for (int i = 0; i < N; i++) {
         result[i] = vector[i] * Tau;
@@ -73,17 +62,11 @@ void s_preparation_perfomance_free(size_t N) {
         matrix[i] = malloc(sizeof(matrix[i]) * N);
     }
     double *vector = malloc(sizeof(vector) * N);
-
     s_fill_matrix_vector(matrix, vector, N);
-
     double *initial_approximation = malloc(sizeof(vector) * N);
-
     s_fill_initial_approximation(initial_approximation, N);
-
     s_solve_equations((const double **) matrix, vector, initial_approximation, N);
-
     //s_print_result(initial_approximation, N);
-
     for (int i = 0; i < N; i++) {
         free(matrix[i]);
     }
@@ -96,11 +79,12 @@ void s_solve_equations(const double **matrix, const double *vector, double *init
     do {
         double *tmp_vect = malloc(sizeof(tmp_vect) * N);
         double *tmp_curr = malloc(sizeof(tmp_curr) * N);
-        for (int i = 0; i < N; i++) {
-            tmp_vect[i] = 0;
-        }
 #pragma omp parallel
         {
+#pragma omp for
+            for (int i = 0; i < N; i++) {
+                tmp_vect[i] = 0;
+            }
             s_multiplication_matrix_vector(matrix, initial_approximation, tmp_vect, N);
 #pragma omp barrier
             s_subtracting_vectors(tmp_vect, vector, N);
