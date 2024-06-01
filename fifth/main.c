b7ece3f29adba89c827688925e8a56e830e248a7
savilovaae <a.savilova@g.nsu.ru>
2024-06-02 01:28:10 +0700

    init

--- fifth/main.c
+++ fifth/main.c
@@ -6,8 +6,7 @@
 
 #define MAX_TASKS 900
 #define ITERATIONS_COUNT 40
-#define L 2
-
+#define MY_MPI_TAG 123
 int *tasks;
 int size;
 int rank;
@@ -16,14 +15,16 @@ double *times;
 pthread_mutex_t mutex;
 
 struct job_requester {
-    int tasks_count;
-    int part_of_res;
+    int tasks_done;
+    int curr_res;
 };
 
+// Функция для выполнения заданий ok
 struct job_requester do_job(int length) {
     int res = 0;
-    int tasks_count = 0;
+    int tasks_done = 0;
     struct job_requester req;
+
     while (1) {
         pthread_mutex_lock(&mutex);
         if (offset >= length) {
@@ -36,29 +37,34 @@ struct job_requester do_job(int length) {
         int weight = tasks[current_offset];
 
         for (int j = 0; j < weight; j++) {
-            res += (int)sqrt(j);
+            res += (int) sqrt(j);
         }
-        tasks_count++;
+        tasks_done++;
     }
-
-    req.part_of_res = res;
-    req.tasks_count = tasks_count;
+    req.curr_res = res;
+    req.tasks_done = tasks_done;
     return req;
 }
 
+// Функция для инициализации задач, установление веса задач. волна задач
 void set_tasks(int iter_count) {
     for (int i = 0; i < size * MAX_TASKS; i++) {
-        tasks[i] = abs(50 - i % 100) * abs(rank - iter_count % size) * L;
+        tasks[i] = abs(50 - i % 100) * abs(rank - iter_count % size);
     }
 }
 
+// Функция для запроса задач у других процессов
 struct job_requester request_tasks() {
+    struct job_requester final_result = {0, 0};
+
     for (int i = 0; i < size; i++) {
-        if (i == rank) continue;
+        if (i == rank) { 
+            continue; 
+        }
         int req_code = 0;
         int help;
 
-        MPI_Send(&req_code, 1, MPI_INT, i, 333, MPI_COMM_WORLD);
+        MPI_Send(&req_code, 1, MPI_INT, i, MY_MPI_TAG, MPI_COMM_WORLD);
         MPI_Recv(&help, 1, MPI_INT, i, 3333, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
         if (help > 0) {
@@ -67,48 +73,57 @@ struct job_requester request_tasks() {
             pthread_mutex_lock(&mutex);
             offset = 0;
             pthread_mutex_unlock(&mutex);
-        }
 
-        return do_job(help);
+            struct job_requester partial_result = do_job(help);
+            final_result.curr_res += partial_result.curr_res;
+            final_result.tasks_done += partial_result.tasks_done;
+        }
     }
+
+    return final_result;
 }
 
-void find_max_and_min(const double* arr, int len, double* max, double* min) {
-    *max = *min = arr[0];
+// Функция для нахождения максимального и минимального значений
+void find_max_and_min(const double *array, int len, double *max, double *min) {
+    *max = *min = array[0];
     for (int i = 1; i < len; i++) {
-        if (arr[i] > *max) {
-            *max = arr[i];
+        if (array[i] > *max) {
+            *max = array[i];
         }
-        if (arr[i] < *min) {
-            *min = arr[i];
+        if (array[i] < *min) {
+            *min = array[i];
         }
     }
 }
 
+// Основная функция выполнения задач
 void do_tasks() {
     double start = MPI_Wtime();
     struct job_requester res1 = do_job(size * MAX_TASKS);
     struct job_requester res2 = request_tasks();
     double end = MPI_Wtime();
 
-    int tasks_count = res1.tasks_count + res2.tasks_count;
-    int part_of_res = res1.part_of_res + res2.part_of_res;
+    int tasks_done = res1.tasks_done + res2.tasks_done;
+    int curr_res = res1.curr_res + res2.curr_res;
     int res = 0;
-    MPI_Allreduce(&part_of_res, &res, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+
+    MPI_Allreduce(&curr_res, &res, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+
     double work_time = end - start;
-    printf("Rank: %d,  tasks done: %d, res = %d, time: %f\n", rank, tasks_count, res, work_time);
+    fprintf(stdout, "rank: %d, | tasks done: %d | res = %d | time: %f \n", rank, tasks_done, res, work_time);
+
     MPI_Gather(&work_time, 1, MPI_DOUBLE, times, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
     if (rank == 0) {
-        double max;
-        double min;
+        double max, min;
         find_max_and_min(times, size, &max, &min);
         double disbalance_time = max - min;
-        printf("Disbalance time: %f, proportion: %f\n", disbalance_time, disbalance_time / max * 100);
+        fprintf(stdout, "disbalance time: %f \n", disbalance_time);
     }
 }
 
-void* work(void* arg) {
+// Функция потока для выполнения задач
+void *work(void *arg) {
     for (int i = 0; i < ITERATIONS_COUNT; i++) {
         pthread_mutex_lock(&mutex);
         offset = 0;
@@ -118,16 +133,17 @@ void* work(void* arg) {
     }
     int stop_code = 1;
     MPI_Barrier(MPI_COMM_WORLD);
-    MPI_Send(&stop_code, 1, MPI_INT, rank, 333, MPI_COMM_WORLD);
+    MPI_Send(&stop_code, 1, MPI_INT, rank, MY_MPI_TAG, MPI_COMM_WORLD);
     return NULL;
 }
 
-void* listen(void* arg) {
+// Функция потока для прослушивания запросов на задачи
+void *listen(void *arg) {
     while (1) {
         int req_code;
         MPI_Status st;
 
-        MPI_Recv(&req_code, 1, MPI_INT, MPI_ANY_SOURCE, 333, MPI_COMM_WORLD, &st);
+        MPI_Recv(&req_code, 1, MPI_INT, MPI_ANY_SOURCE, MY_MPI_TAG, MPI_COMM_WORLD, &st);
 
         if (req_code == 1) {
             break;
@@ -136,7 +152,7 @@ void* listen(void* arg) {
         size_t length = size * MAX_TASKS;
 
         pthread_mutex_lock(&mutex);
-        int new_offset = offset + (int)((int)(length - offset) * 0.1);
+        int new_offset = offset + (int) ((length - offset) * 0.1);
         int tasks_length = new_offset - offset;
         pthread_mutex_unlock(&mutex);
 
@@ -153,20 +169,20 @@ void* listen(void* arg) {
     return NULL;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
     int provided;
     MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &provided);
     if (provided != MPI_THREAD_MULTIPLE) {
-        printf("Invalid thread level support\n");
+        fprintf(stdout, "invalid thread level support\n");
         exit(-1);
     }
 
     double start = MPI_Wtime();
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    tasks = (int*) malloc(size * MAX_TASKS * sizeof(int));
+    tasks = (int *) malloc(size * MAX_TASKS * sizeof(int));
     if (rank == 0) {
-        times = (double*) malloc(size * sizeof(double));
+        times = (double *) malloc(size * sizeof(double));
     }
 
     pthread_t worker, listener;
@@ -184,7 +200,7 @@ int main(int argc, char* argv[]) {
     if (rank == 0) {
         free(times);
         double end = MPI_Wtime();
-        printf("Time: %f\n", end - start);
+        fprintf(stdout, "time: %f\n", end - start);
     }
 
     MPI_Finalize();
